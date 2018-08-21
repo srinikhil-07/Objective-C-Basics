c9b5c78942e0e3230ca5eb0a994149d16b69ded7
NikhilDuggirala <nikhilduggirala95@gmail.com>
2018-08-21 12:18:02 +0530

    plist which triggers on load

---
+++ DelegatingClass.h
@@ -0,0 +1,13 @@
+//
+//  DelegatingClass.h
+//  Basics
+//
+//  Created by sri-7348 on 21/08/18.
+//  Copyright © 2018 sri-7348. All rights reserved.
+//
+
+#ifndef DelegatingClass_h
+#define DelegatingClass_h
+
+
+#endif /* DelegatingClass_h */
---
+++ DelegatingClass.m
@@ -0,0 +1,9 @@
+//
+//  DelegatingClass.m
+//  Basics
+//
+//  Created by sri-7348 on 21/08/18.
+//  Copyright © 2018 sri-7348. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
